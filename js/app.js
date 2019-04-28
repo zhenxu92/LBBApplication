61b785e369adaf21e8182d6a2668b8ff37493b76
zhenxu92 <zhenxu0213@gmail.com>
2019-04-27 22:04:42 -0700

    Add signature canvas

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var canvas = wrapper.querySelector("canvas");
+var signaturePad = new SignaturePad(canvas, {
+  // It's Necessary to use an opaque color when saving image as JPEG;
+  // this option can be omitted if only saving as PNG or SVG
+  backgroundColor: 'rgb(255, 255, 255)'
+});
