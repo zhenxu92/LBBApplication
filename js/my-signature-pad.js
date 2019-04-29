d74363e88dbedd8b9837935b7a668b4e304debf2
Zhen Xu <zxaamzn@amazon.com>
2019-04-28 18:53:40 -0700

    First stable version

---
+++ js/my-signature-pad.js
@@ -0,0 +1,9 @@
+var pxSignaturePad = new SignaturePad(document.getElementById('px-signature-pad'), {
+  backgroundColor: 'rgba(255, 255, 255, 0)',
+  penColor: 'rgb(0, 0, 0)'
+});
+
+var ttSignaturePad = new SignaturePad(document.getElementById('tt-signature-pad'), {
+  backgroundColor: 'rgba(255, 255, 255, 0)',
+  penColor: 'rgb(0, 0, 0)'
+});
